ee127bbc39a3aeca01d67d72a0234d7fd944ce42
Junyan Yang <1310204834@qq.com>
2022-05-23 23:00:18 +0800

    2022.5.23

---
+++ 739.每日温度.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=739 lang=cpp
+ *
+ * [739] 每日温度
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures){
+        vector<int> dailyTemperatures(vector<int> & T){
+            // 递增栈
+            stack<int> st;
+    vector<int> result(T.size(), 0);
+    st.push(0);
+    for (int i = 1; i < T.size(); i++)
+    {
+        if (T[i] < T[st.top()])
+        { // 情况一
+            st.push(i);
+        }
+        else if (T[i] == T[st.top()])
+        { // 情况二
+            st.push(i);
+        }
+        else
+        {
+            while (!st.empty() && T[i] > T[st.top()])
+            { // 情况三
+                result[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+    }
+    return result;
+}
+}
+;
+// @lc code=end
