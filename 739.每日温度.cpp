c804b55f4ab47ece036dd23137d27bd1584684ec
Junyan Yang <1310204834@qq.com>
2022-05-23 23:05:01 +0800

    2022.5.23

--- 739.每日温度.cpp
+++ 739.每日温度.cpp
@@ -8,34 +8,33 @@
 class Solution
 {
 public:
-    vector<int> dailyTemperatures(vector<int> &temperatures){
-        vector<int> dailyTemperatures(vector<int> & T){
-            // 递增栈
-            stack<int> st;
-    vector<int> result(T.size(), 0);
-    st.push(0);
-    for (int i = 1; i < T.size(); i++)
+    vector<int> dailyTemperatures(vector<int> &T)
     {
-        if (T[i] < T[st.top()])
-        { // 情况一
-            st.push(i);
-        }
-        else if (T[i] == T[st.top()])
-        { // 情况二
-            st.push(i);
-        }
-        else
+        // 递增栈
+        stack<int> st;
+        vector<int> result(T.size(), 0);
+        st.push(0);
+        for (int i = 1; i < T.size(); i++)
         {
-            while (!st.empty() && T[i] > T[st.top()])
-            { // 情况三
-                result[st.top()] = i - st.top();
-                st.pop();
+            if (T[i] < T[st.top()])
+            { // 情况一
+                st.push(i);
             }
-            st.push(i);
+            else if (T[i] == T[st.top()])
+            { // 情况二
+                st.push(i);
+            }
+            else
+            {
+                while (!st.empty() && T[i] > T[st.top()])
+                { // 情况三
+                    result[st.top()] = i - st.top();
+                    st.pop();
+                }
+                st.push(i);
+            }
         }
+        return result;
     }
-    return result;
-}
-}
-;
+};
 // @lc code=end
